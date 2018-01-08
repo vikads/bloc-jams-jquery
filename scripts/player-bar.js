4e9baf2a1fee373da964ffc1145458f8b3e66c2d
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-08 14:05:04 -0800

    checkpont 20/15 completed, current time, song's playback position, SetInterval Function

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -24,6 +24,7 @@
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
 }
