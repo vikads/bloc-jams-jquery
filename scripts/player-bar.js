eb13339348d3d12abdfc16de003155fa74c88411
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-08 13:41:10 -0800

    time slider moves, working with percentage

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -17,5 +17,9 @@
 
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
   }, 1000);
 }
