e92820c19eeb2dd994afc094615e52f330675532
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-08 14:31:48 -0800

    assignment 14 completed, previous button works

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,10 +16,11 @@
   });
 
   $('button#previous').on('click', function() {
-    if(player.playSate !== 'playing') { return; }
+   if(player.playState !== 'playing') { return; }
 
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex === 0) { return; } /// tu powinien byc index pierwszej piisenki na liscie czyli [0]
+    if (previousSongIndex < 0) { return; } /// tu powinien byc index pierwszej piisenki na liscie czyli [0]
 
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
