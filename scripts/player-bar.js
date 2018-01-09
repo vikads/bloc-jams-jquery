ac461030d5d24d422af0e5192be9bcfad31369a1
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-01-09 10:43:26 -0800

    you can set the volume now

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -31,7 +31,7 @@
       player.skipTo(event.target.value);
     });
 
-   $('#volume-control input')on ('input', function (event) {
+   $('#volume-control input').on('input', function (event) {
     player.setVolume(event.target.value);
   });
 
